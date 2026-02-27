8c5c3528b1c4ee89bcb6a585f71444a0c1cd49a2
Stefani Langehennig <stefani.langehennig@colorado.edu>
2026-02-27 13:15:58 -0700

    fix build remove local favicon ref

--- tracker/observablehq.config.js
+++ tracker/observablehq.config.js
@@ -7,4 +7,17 @@ export default {
   ],
   base: "/tracker/",
   style: "style.css",
+  theme: "light",
+  header: `<div class="caid-topbar">
+  <a class="caid-topbar-brand" href="https://du-caid.github.io/">
+    <img src="https://du-caid.github.io/assets/img/CAID_logo_square_compressed.png" alt="CAID logo" width="32" height="32">
+    <span>DU CAID</span>
+  </a>
+  <nav class="caid-topbar-nav">
+    <a href="https://du-caid.github.io/about.html">About</a>
+    <a href="https://du-caid.github.io/projects.html">Projects</a>
+    <a href="https://du-caid.github.io/people.html">People</a>
+    <a href="https://du-caid.github.io/contact.html">Contact</a>
+  </nav>
+</div>`,
 };
